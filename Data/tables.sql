af5d2c2d6b0391726432e37284eba1904a70f518
EnLuring <enluring@gmail.com>
2023-03-23 20:46:50 +0100

    Added tabels sql file

---
+++ Data/tables.sql
@@ -0,0 +1,59 @@
+CREATE TABLE drivers (
+    id SERIAL PRIMARY KEY,
+    team_id INTEGER REFERENCES teams (id),
+    driver_no INTEGER,
+    forename VARCHAR(50),
+    surname VARCHAR(50),
+    nationality VARCHAR(50),
+    dob DATE
+
+);
+
+CREATE TABLE teams (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE,
+    nationality VARCHAR(50)
+);
+
+
+-- Create the circuits table
+CREATE TABLE circuits (
+    circuit_id SERIAL PRIMARY KEY,
+    circuit_ref VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    location VARCHAR(255),
+    country VARCHAR(255),
+    geolocation POINT,
+    altitude INT,
+    url VARCHAR(255)
+);
+
+
+CREATE TABLE races (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    date DATE,
+    circuit INTEGER REFERENCES circuits (circuit_id)
+);
+
+CREATE TABLE lap_times (
+    race_id INTEGER REFERENCES races (id),
+    driver_id INTEGER REFERENCES drivers (id),
+    lap INTEGER,
+    time VARCHAR(20),
+    session_type VARCHAR(10)
+);
+
+CREATE TABLE results (
+    race_id INTEGER REFERENCES races (id),
+    driver_id INTEGER REFERENCES drivers (id),
+    team_id INTEGER REFERENCES teams (id),
+    start_grid INTEGER,
+    position INTEGER,
+    points FLOAT,
+    laps INTEGER,
+    time VARCHAR(20),
+    fastest_lap INTEGER,
+    fastest_lap_time VARCHAR(20)
+);
+
