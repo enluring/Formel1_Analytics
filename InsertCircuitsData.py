1fcedb96d11a16d0074612bce23fa057750645bc
EnLuring <enluring@gmail.com>
2023-03-25 00:08:57 +0100

    lagt inn script for å legge inn circuits

---
+++ InsertCircuitsData.py
@@ -0,0 +1,36 @@
+import csv
+import psycopg2
+import os
+
+# set up database connection
+conn_params = {
+    "host": "172.19.0.2",
+    "port": "5432",
+    "database": "f1db",
+    "user": "f1admin",
+    "password": "f1admin"
+}
+cursor = conn.cursor()
+
+# Get the path to the SQL file
+script_dir = os.path.dirname(__file__)
+file_path = os.path.join(script_dir, 'data', 'circuits.csv')
+
+
+# open CSV file and read contents into a list
+with open(file_path, 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader) # skip header row
+    rows = [tuple(row) for row in reader]
+
+# generate placeholders for SQL query
+placeholders = ','.join(['%s'] * len(rows[0]))
+
+# insert data into database
+query = f"INSERT INTO mytable (col1, col2, col3) VALUES ({placeholders})"
+cursor.executemany(query, rows)
+conn.commit()
+
+# close database connection
+cursor.close()
+conn.close()
