52c9a3e9fc0b62d2f06ae21aa929dcb64435f6f8
enluring <enluring@gmail.com>
2023-03-29 19:03:42 +0200

    Oppdatert insertcsv script

--- InsertDataToTable_v2.py
+++ InsertDataToTable_v2.py
@@ -35,7 +35,9 @@ for file_name in os.listdir(DATA_DIR):
         for row in reader:
             # Replace '\N' values with None or an empty string
             row = [None if x == '\\N' else x for x in row]
-            row = [None if x == '""' else x for x in row]
+            row = [None if x == r' ' else x for x in row]
+            #row = [None if x == '""' else x for x in row]
+            #row = [float(val) if val.replace(".", "", 1).isdigit() else None for val in row]
             try:
                 rows_to_insert.append(tuple(row))
             except ValueError:
