9a3012d62d1ac32955a5211fd0943ee0e13c77c9
enluring <enluring@gmail.com>
2023-06-15 23:46:47 +0200

    oppdatert variabler

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+# Variables
+PG_DUMP_FILE="/docker-entrypoint-initdb.d/f1db_dump_latest.sql"
+DB_NAME="f1db"
+DB_USER="f1admin"
+POSTGRES_PASSWORD="f1admin"
+POSTGRES_HOST="f1_db"
+POSTGRES_USER="f1admin"
+
 # Wait for PostgreSQL container to be ready
 until PGPASSWORD="$POSTGRES_PASSWORD" psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -c '\q' >/dev/null 2>&1; do
     echo "Waiting for PostgreSQL container to be ready..."
     sleep 1
 done
 
-# Variables
-PG_DUMP_FILE="/docker-entrypoint-initdb.d/f1db_dump_latest.sql"
-DB_NAME="f1db"
-DB_USER="f1admin"
+
 
 # Create the database
 psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -c "CREATE DATABASE $DB_NAME;"
